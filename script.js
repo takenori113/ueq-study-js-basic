774758d944505e17fdcacc6f1c98bced2a3d4727
Takenori <takenori113@gmail.com>
2024-04-17 11:13:24 +0900

    指摘箇所を修正しました

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ループ・配列・データ型の問題
 export const func1 = (n) => {
-  if (n >= 0 || n & 1 === 0) {
+  if (Number.isInteger(n)) {
     const reslut = [];
     for (let i = 0; i < n; i++) {
       reslut.push(1 + i);
@@ -12,17 +12,17 @@ export const func1 = (n) => {
 
 // 条件分岐・データ型の問題
 export const func2 = (n) => {
-  if (!(n >= 0 || n & 1 === 0)) {
+  if (!(Number.isInteger(n)&&n>0)) {
     return "error";
   }
-  if (n % 3 === 0 && n % 5 !== 0 && n >= 3) {
-    return "Fizz";
-  } else if (n % 3 !== 0 && n % 5 === 0 && n >= 5) {
-    return "Buzz";
-  } else if (n % 3 === 0 && n % 5 === 0 && n >= 15) {
+  if (n % 3 === 0 && n % 5 === 0 && n >= 15) {
     return "FizzBuzz";
+  } else if (n % 3 === 0) {
+    return "Fizz";
+  } else if (n % 5 === 0) {
+    return "Buzz";
   } else {
-    return n;
+    return `${n}`;
   }
 
 
@@ -33,7 +33,7 @@ const main = () => {
   const result1 = func1(5);
   console.log(result1);
   // func2の実行例
-  const result2 = func2(1.1);
+  const result2 = func2(1);
   console.log(result2);
 };
 
