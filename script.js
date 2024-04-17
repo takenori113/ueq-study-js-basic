0abaa594021a90d3a3c135b9324daace44dcd63b
Takenori <takenori113@gmail.com>
2024-04-17 13:19:14 +0900

    指摘箇所を修正しました２

--- script.js
+++ script.js
@@ -12,10 +12,9 @@ export const func1 = (n) => {
 
 // 条件分岐・データ型の問題
 export const func2 = (n) => {
-  if (!(Number.isInteger(n)&&n>0)) {
-    return "error";
-  }
-  if (n % 3 === 0 && n % 5 === 0 && n >= 15) {
+    if (!Number.isInteger(n)) return "error";
+  if (n <= 0) return "error";
+  if (n % 3 === 0 && n % 5 === 0) {
     return "FizzBuzz";
   } else if (n % 3 === 0) {
     return "Fizz";
@@ -33,7 +32,7 @@ const main = () => {
   const result1 = func1(5);
   console.log(result1);
   // func2の実行例
-  const result2 = func2(1);
+  const result2 = func2(0);
   console.log(result2);
 };
 
